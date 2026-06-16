0b64442cb4aed92685d3388e178e03c18d7a7800
Ankit Mittal <191123870+AnkitnMittal@users.noreply.github.com>
2026-06-16 16:29:46 +0000

    create server

---
+++ t3-http/server.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    /* Set header */
+    res.setHeader('Content-Type', 'text/html');
+
+    /* Set HTML endpoint path based on Web URL */
+    let path = './t3-http/views';
+    switch (req.url) {
+        case '/':
+            path += '/index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += '/about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '/404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    /* Read file and send response */
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server is listening on http://localhost:3000');
+});
